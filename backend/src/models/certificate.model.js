a2de25c1dafd6e730323ab1704bd94cf2e42724c
Jash Bohare <jashbohare@gmail.com>
2026-01-09 22:04:15 +0530

    Adding Certificate Schema

---
+++ backend/src/models/certificate.model.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const certificateSchema = new mongoose.Schema({
+    studentName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    course: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    grade: {
+        type: String,
+        required: true,
+    },
+
+    issueDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+
+    universityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'University',
+        required: true
+    },
+
+    certificateHash: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    transactionHash: {
+        type: String,
+        unique: true,
+        sparse: true
+    },
+
+    qrCode: String,
+    blockchainVerified: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true });
+
+const Certificate = mongoose.model('Certificate', certificateSchema);
+
+export default Certificate;
